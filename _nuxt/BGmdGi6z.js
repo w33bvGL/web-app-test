6e1ea135761532531e82a130c2f40368bfc79b4a
Vahe Sargsyan <w33bv.gl@gmail.com>
2025-06-26 22:02:24 +0400

    Updates

--- _nuxt/BGmdGi6z.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,c as s,o,j as c}from"./O4UKbSSa.js";const r={},a={class:"bg-neutral-100"};function n(e,_){return o(),s("div",a,[c(e.$slots,"default")])}const d=t(r,[["render",n]]);export{d as default};
---
+++ _nuxt/CYWy0ujI.js
@@ -0,0 +1,1 @@
+import{_ as t,c as s,o,j as c}from"./DAvYlofG.js";const r={},a={class:"bg-neutral-100"};function n(e,_){return o(),s("div",a,[c(e.$slots,"default")])}const d=t(r,[["render",n]]);export{d as default};
